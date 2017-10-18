2c95f8b7e8f7835ed32bc8c8869e44620a67fc51
Vantte Kilappa <warma@iki.fi>
2017-10-18 18:22:02 +0300

    Scripts for constructing a strict filter and the strict filter

---
+++ wloot_construct_strict_filter.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+import re
+
+filter_name = "wloot"
+
+filehandle_in  = open(filter_name + ".filter", "r")
+filehandle_out  = open(filter_name + "_strict.filter", "w")
+wloot_filter = filehandle_in.read()
+hide_pattern = re.compile("# Strict\nShow")
+wloot_filter = re.sub(hide_pattern, "Hide", wloot_filter)
+
+filehandle_out.write(wloot_filter)

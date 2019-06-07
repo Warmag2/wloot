112d91213c836cb652d274360ec2474aec311276
Vantte Kilappa <warma@iki.fi>
2019-06-07 23:26:23 +0300

    New league shit

--- wloot_construct_strict_filter.py
+++ wloot_construct_strict_filter.py
@@ -4,8 +4,8 @@ import re
 
 filter_name = "wloot"
 
-filehandle_in  = open(filter_name + ".filter", "r")
-filehandle_out  = open(filter_name + "_strict.filter", "w")
+filehandle_in = open(filter_name + ".filter", "r")
+filehandle_out = open(filter_name + "_strict.filter", "w")
 wloot_filter = filehandle_in.read()
 hide_pattern = re.compile("# Strict\nShow")
 wloot_filter = re.sub(hide_pattern, "Hide", wloot_filter)
